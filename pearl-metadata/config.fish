e97253e8c3ccc8f010663fbaa653f70fa1cb8289
Filippo Squillace <squillace.filippo@gmail.com>
2016-05-01 19:32:49 +0100

    Add path to PATH only once

--- pearl-metadata/config.fish
+++ pearl-metadata/config.fish
@@ -1,4 +1,9 @@
-set MANPATH $MANPATH $PEARL_PKGDIR/man
-set PATH $PATH $PEARL_PKGDIR/bin
+if not contains $PEARL_PKGDIR/bin $PATH
+    set PATH $PATH $PEARL_PKGDIR/bin
+end
+
+if not contains $PEARL_PKGDIR/man $MANPATH
+    set MANPATH $MANPATH $PEARL_PKGDIR/man
+end
 
 # vim: ft=sh
--- pearl-metadata/config.sh
+++ pearl-metadata/config.sh
@@ -1,4 +1,12 @@
-MANPATH=$MANPATH:$PEARL_PKGDIR/man
-PATH=$PATH:${PEARL_PKGDIR}/bin
+
+if [[ $PATH != *"${PEARL_PKGDIR}/bin"* ]]
+then
+    PATH=$PATH:$PEARL_PKGDIR/bin
+fi
+
+if [[ $MANPATH != *"${PEARL_PKGDIR}/man"* ]]
+then
+    MANPATH=$MANPATH:$PEARL_PKGDIR/man
+fi
 
 # vim: ft=sh
