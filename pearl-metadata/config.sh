4a4505ad9bc85c971c43f33d0fe559461680ce29
Filippo Squillace <squillace.filippo@gmail.com>
2016-11-09 19:42:09 +0000

    Move manuals to kobol-extra

--- pearl-metadata/config.sh
+++ pearl-metadata/config.sh
@@ -4,11 +4,6 @@ export KOBOL_VARDIR="$PEARL_PKGVARDIR"
 mkdir -p ${KOBOL_VARDIR}/markdown
 mkdir -p ${KOBOL_VARDIR}/mans/man1
 
-if [[ $KOBOL_MANPATH != *"${PEARL_PKGDIR}/mans"* ]]
-then
-    export KOBOL_MANPATH="$PEARL_PKGDIR/mans:$KOBOL_MANPATH"
-fi
-
 if [[ $KOBOL_MANPATH != *"${PEARL_PKGVARDIR}/mans"* ]]
 then
     export KOBOL_MANPATH="$PEARL_PKGVARDIR/mans:$KOBOL_MANPATH"
--- tests/integ-tests/integ-tests-common.sh
+++ tests/integ-tests/integ-tests-common.sh
@@ -2,8 +2,6 @@
 
 kobol --help
 
-kobol -P cat string | head
-
 echo -e "## Kobol ##\nKobol is a tool for creating manuals via markdown files." > \
     ~/.config/pearl/var/default/test/markdown/example.md
 
