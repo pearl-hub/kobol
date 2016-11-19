59465aa5bffbd29e8fc50fa7ddb6e6d936b4251b
Filippo Squillace <squillace.filippo@gmail.com>
2016-11-19 12:10:09 +0000

    :art: Improve integ tests

--- tests/integ-tests/integ-tests-common.sh
+++ tests/integ-tests/integ-tests-common.sh
@@ -6,3 +6,7 @@ echo -e "## Kobol ##\nKobol is a tool for creating manuals via markdown files." 
     ~/.config/pearl/var/default/test/markdown/example.md
 
 kobol -P cat example
+
+pearl update test
+
+pearl remove test
--- tests/integ-tests/integ-tests.sh
+++ tests/integ-tests/integ-tests.sh
@@ -2,15 +2,8 @@
 
 set -e
 
-if [ -z "$1" ]
-then
-    echo "ERROR: To run the integ tests you must specify the Pearl location."
-    echo "For instance: $0 ~/.local/share/pearl"
-    exit 33
-fi
+source $HOME/.bashrc
 
-export PEARL_ROOT="$1"
-
-[ -d $PEARL_HOME/packages/default/test ] || { echo "The package test does not exist after install"; exit 1; }
+pearl install test
 
 source ./tests/integ-tests/integ-tests-common.sh
